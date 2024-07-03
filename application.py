c07552803388690130fb5756cd3f4eaf7895dad6
KRA <oumarkra@yahoo.fr>
2024-07-03 19:15:55 +0000

    fonction chebyshev_distance1

--- application.py
+++ application.py
@@ -2,6 +2,10 @@ def chebyshev_distance(p1, p2):
     """Docstring..."""
     return max(abs(x - y) for x, y in zip(p1, p2))
 
+def chebyshev_distance1(p1, p2):
+    return max([abs(x - y) for x, y in zip(p1, p2)])
+
+
 if __name__ == "__main__":
     point1 = (1, 2, 3)
     point2 = (4, 5, 6)
