4eab7e0a1ed21fdd9c79cea0bd2778a3c531cf44
KRA <oumarkra@yahoo.fr>
2024-07-03 19:20:25 +0000

    fonction chebyshev_distanc21

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+import numpy as np
 def chebyshev_distance(p1, p2):
     """Docstring..."""
     return max(abs(x - y) for x, y in zip(p1, p2))
@@ -6,8 +7,16 @@ def chebyshev_distance1(p1, p2):
     return max([abs(x - y) for x, y in zip(p1, p2)])
 
 
+def chebyshev_distance2(p1, p2):
+    return np.max(np.abs(np.array(p1) - np.array(p2)))
+
+
 if __name__ == "__main__":
     point1 = (1, 2, 3)
     point2 = (4, 5, 6)
     distance = chebyshev_distance(point1, point2)
     print("Chebyshev Distance:", distance)
+    distance1 = chebyshev_distance1(point1, point2)
+    print("Chebyshev Distance1:", distance1)
+    distance2 = chebyshev_distance2(point1, point2)
+    print("Chebyshev Distance2:", distance2)
